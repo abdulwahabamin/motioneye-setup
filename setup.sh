2f4c4e3dee503eba21edc30c28dc37b6d091d4d8
abdulwahabamin <abdulwahabamin@yahoo.com>
2020-09-15 18:58:21 +0100

    fixed installation errors

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
  #!/bin/bash
- sudo apt-get install -y motion ffmpeg v4l-utils python-pip python-dev python-setuptools curl libssl-dev libcurl4-openssl-dev libjpeg-dev libz-dev
- sudo pip install motioneye==0.41
+ sudo apt-get install -y motion ffmpeg v4l-utils
+ sudo apt-get install -y python-pip python-dev python-setuptools curl libssl-dev libcurl4-openssl-dev libjpeg-dev libz-dev
+ sudo apt-get install -y python-pillow
+ sudo pip install jinja2 tornado pycurl motioneye==0.41
  sudo mkdir -p /etc/motioneye
  sudo cp /usr/local/share/motioneye/extra/motioneye.conf.sample /etc/motioneye/motioneye.conf
  sudo mkdir -p /var/lib/motioneye
